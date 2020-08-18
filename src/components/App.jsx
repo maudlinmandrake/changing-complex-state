459ebdec21c832fea13fa041a14e318003fe992e
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-18 11:02:21 -0400

    Update first AND last name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+
+  function updateFName() {
+    const firstName = event.target.value;
+    setFName(firstName);
+  }
+
+  function updateLName() {
+    const lastName = event.target.value;
+    setLName(lastName);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fName + " " + lName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          name="fName"
+          onChange={updateFName}
+          placeholder="First Name"
+          value={fName}
+        />
+        <input
+          name="lName"
+          onChange={updateLName}
+          placeholder="Last Name"
+          value={lName}
+        />
         <button>Submit</button>
       </form>
     </div>
